620828e338b5e7b385de2574f34af91fd9a941a2
Aaryak <82655626+AaryakCreator@users.noreply.github.com>
2021-12-26 17:25:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,7 +73,7 @@ function draw() {
 //   balloon.y = height.y;
 // }
 
- function readHeight(data){
+function readHeight(data){
    height = data.val();
    balloon.x = height.x;
    balloon.y = height.y;
